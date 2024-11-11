d25098af16548c12add6f8c8b9acf7b0e7b00a78
Richard <r.martinezdk@gmail.com>
2024-11-11 16:47:49 -0300

    Switch from SqlServer to sqlite

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<GameContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<GameService>();
 
@@ -29,8 +29,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
